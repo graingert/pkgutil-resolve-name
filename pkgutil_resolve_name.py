6d9245a87ff4bb76996d9ea9f766c0a2dcfbcd28
Thomas Grainger <tagrain@gmail.com>
2020-02-18 10:38:01 +0000

    publish to pypi

--- pkgutil_resolve_name.py
+++ pkgutil_resolve_name.py
@@ -1,5 +1,39 @@
+"""
+Resolve a name to an object.
+
+It is expected that `name` will be a string in one of the following
+formats, where W is shorthand for a valid Python identifier and dot stands
+for a literal period in these pseudo-regexes:
+
+W(.W)*
+W(.W)*:(W(.W)*)?
+
+The first form is intended for backward compatibility only. It assumes that
+some part of the dotted name is a package, and the rest is an object
+somewhere within that package, possibly nested inside other objects.
+Because the place where the package stops and the object hierarchy starts
+can't be inferred by inspection, repeated attempts to import must be done
+with this form.
+
+In the second form, the caller makes the division point clear through the
+provision of a single colon: the dotted name to the left of the colon is a
+package to be imported, and the dotted name to the right is the object
+hierarchy within that package. Only one import is needed in this form. If
+it ends with the colon, then a module object is returned.
+
+The function will return an object (which might be a module), or raise one
+of the following exceptions:
+
+ValueError - if `name` isn't in a recognised format
+ImportError - if an import failed when it shouldn't have
+AttributeError - if a failure occurred when traversing the object hierarchy
+                 within the imported package to get to the desired object)
+"""
+
 import importlib
 import re
+
+__version__ = "0.0.0.a0"
 
 _DOTTED_WORDS = r'[a-z_]\w*(\.[a-z_]\w*)*'
 _NAME_PATTERN = re.compile(f'^({_DOTTED_WORDS})(:({_DOTTED_WORDS})?)?$', re.I)
@@ -7,37 +41,6 @@ del _DOTTED_WORDS
 
 
 def resolve_name(name):
-    """
-    Resolve a name to an object.
-
-    It is expected that `name` will be a string in one of the following
-    formats, where W is shorthand for a valid Python identifier and dot stands
-    for a literal period in these pseudo-regexes:
-
-    W(.W)*
-    W(.W)*:(W(.W)*)?
-
-    The first form is intended for backward compatibility only. It assumes that
-    some part of the dotted name is a package, and the rest is an object
-    somewhere within that package, possibly nested inside other objects.
-    Because the place where the package stops and the object hierarchy starts
-    can't be inferred by inspection, repeated attempts to import must be done
-    with this form.
-
-    In the second form, the caller makes the division point clear through the
-    provision of a single colon: the dotted name to the left of the colon is a
-    package to be imported, and the dotted name to the right is the object
-    hierarchy within that package. Only one import is needed in this form. If
-    it ends with the colon, then a module object is returned.
-
-    The function will return an object (which might be a module), or raise one
-    of the following exceptions:
-
-    ValueError - if `name` isn't in a recognised format
-    ImportError - if an import failed when it shouldn't have
-    AttributeError - if a failure occurred when traversing the object hierarchy
-                     within the imported package to get to the desired object)
-    """
     m = _NAME_PATTERN.match(name)
     if not m:
         raise ValueError(f'invalid format: {name!r}')
